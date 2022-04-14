e952b32a681195e6813d66ef1e5d3e6c1cee57c0
RemLampa <rem@remlampa.com>
2022-04-14 16:00:37 +0800

    :zap: self-host Inter font

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import GlobalStyles from '../GlobalStyles';
-import Header from '../Header';
-import Hero from '../Hero';
-import MainContent from '../MainContent';
-import Footer from '../Footer';
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/variable.css";
+
+import GlobalStyles from "../GlobalStyles";
+import Header from "../Header";
+import Hero from "../Hero";
+import MainContent from "../MainContent";
+import Footer from "../Footer";
 
 const App = () => {
   return (
